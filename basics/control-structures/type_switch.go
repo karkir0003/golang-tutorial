a45d9f3561369422172f07b98a651ed3a4599687
karkir0003 <karkir0003@gmail.com>
2026-02-01 22:24:07 -0500

    type switcher in go

--- basics/control-structures/type_switch.go
+++ basics/control-structures/type_switch.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
+// GO BEST PRACTICE: Try to define type of your function! Go forces YOU to define the typing. Casting is done on
+// a best effort basis
+func processDay(day int) {
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	default:
+		fmt.Println("Unknown weekday")
+	}
+	return
+}
+
 func main() {
 	/**
 	TODO: Write a function that takes in a variable day.
@@ -8,4 +30,17 @@ func main() {
 
 	If day is another type, just print the type of the day
 	*/
+
+	var day any // generically typed variable with type determination done by us for concrete use case
+
+	// pitfall: If you do day := 4, go compiler INFERS the type to ALWAYS be an int
+	day = true
+
+	// day.(type) is only meaningful for "any" type
+	switch v := day.(type) {
+	case int:
+		processDay(v)
+	default:
+		fmt.Printf("Input type is: %T with value %v", v, v) // how to printf type of variable
+	}
 }
