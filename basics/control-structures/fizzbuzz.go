e28776ee59b3f4b6e618dd00bef41d9833912e4a
karkir0003 <karkir0003@gmail.com>
2026-02-04 16:43:00 -0500

    fizzbuzz with while loop

--- basics/control-structures/fizzbuzz.go
+++ basics/control-structures/fizzbuzz.go
@@ -26,4 +26,24 @@ func main() {
 			fmt.Println(i)
 		}
 	}
+
+	// Same solution but with while loop.
+	// Go doesn't have a dedicated while keyword
+	currString = ""
+	ctr := 1
+	fmt.Println("============ While Loop ===========")
+	for ctr <= 100 {
+		currString = ""
+		if ctr%3 == 0 {
+			currString += "Fizz"
+		} else if ctr%5 == 0 {
+			currString += "Buzz"
+		}
+		if len(currString) > 0 {
+			fmt.Println(currString)
+		} else {
+			fmt.Println(ctr)
+		}
+		ctr += 1
+	}
 }
